e7c87a014517f1733884d9884468acc558b36561
Martin <martinpetit1998@gmail.com>
2022-03-16 18:36:49 +0100

    ajout design webpage

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,20 @@ const sass = require('gulp-sass')(require('sass'));
 
 gulp.task('browserSync', function() {
     browserSync.init(null, {
-      server: {
-        baseDir: './src'
-      },
+        server: {
+            baseDir: './src'
+        },
     });
 })
 
 gulp.task('sass', function(){
     return gulp.src('./src/scss/style.scss')
-      .pipe(sass())    // ici on utisise gulp-sass
-      .pipe(gulp.dest('dist/style.css'))
-      .pipe(browserSync.stream({stream: true}));
+        .pipe(sass())    // ici on utisise gulp-sass
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream({stream: true}));
 });
 
 gulp.task('watch', function (){
-    gulp.watch('./src/scss/style.scss', { events: 'all' }, gulp.series(['sass'])); 
+    gulp.watch('./src/scss/style.scss', { events: 'all' }, gulp.series(['sass']));
     // Other watchers
-  })
+})
