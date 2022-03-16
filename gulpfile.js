7f86d1a99994e9cfdd3bc2e15d9cf20ace612bac
edgcht <edouard.gachet@hetic.net>
2022-03-16 18:07:30 +0100

    install gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,24 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    cb();
-}
+const gulp = require('gulp');
 
-exports.default = defaultTask
+var browserSync = require('browser-sync');
+const sass = require('gulp-sass')(require('sass'));
+
+gulp.task('browserSync', function() {
+    browserSync.init(null, {
+      server: {
+        baseDir: './src'
+      },
+    });
+})
+
+gulp.task('sass', function(){
+    return gulp.src('./src/scss/style.scss')
+      .pipe(sass())    // ici on utisise gulp-sass
+      .pipe(gulp.dest('dist/style.css'))
+      .pipe(browserSync.stream({stream: true}));
+});
+
+gulp.task('watch', function (){
+    gulp.watch('./src/scss/style.scss', { events: 'all' }, gulp.series(['sass'])); 
+    // Other watchers
+  })
